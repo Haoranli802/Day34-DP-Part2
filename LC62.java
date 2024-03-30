956538ef235c1842cc49a113043f28ff8d39a2ad
Haoran Li (Evan) <65979806+Haoranli802@users.noreply.github.com>
2024-03-30 18:55:36 +0800

    Update LC62.java

--- LC62.java
+++ LC62.java
@@ -16,3 +16,19 @@ class Solution {
     }
 }
 //O(M * N), O(M * N)
+
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[] dp = new int[n];
+        for(int i = 0; i < n; i++){
+            dp[i] = 1;
+        }
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
+                dp[j] = dp[j] + dp[j - 1];
+            }
+        }
+        return dp[n - 1];
+    }
+}
+//O(N), O(N)
